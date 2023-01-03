63dc6586b8cbe701fe77217bbce9caf1ba7cff46
Goncalo Guerra <guerra.m.goncalo@protonmail.ch>
2023-01-03 18:45:18 +0000

    Trabalho V1 Feito. SIUUUUU

--- rpi/opclient.py
+++ rpi/opclient.py
@@ -1,37 +1,78 @@
-from opcua import Server
-from random import randint
-import datetime
+from paho.mqtt import client as mqtt_client
+import sys
 import time
+from opcua import ua, Server
+import random
 
-server = Server()
+sys.path.insert(0, "..")
 
-url = "opc.tcp://192.168.1.203:4840"
-server.set_endpoint(url)
+broker = "localhost"
+port = 1883
+topic = "testTopic"
 
-name = "OPCUA_SIMULATION_SERVER"
-addspace = server.register_namespace(name)
+client_id = f"python-mqtt-{random.randint(0, 1000)}"
+message = "6.7"
 
-node = server.get_objects_node()
 
-Param = node.add_object(addspace, "Parameters")
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
 
-Temp = Param.add_variable(addspace, "Temperature", 0)
-Hum = Param.add_variable(addspace, "Humidade", 0)
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
 
-Temp.set_writable()
-Hum.set_writable()
 
-server.start()
-print("Server started at {}".format(url))
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        global message
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        message = msg.payload.decode()
 
-while True:
+    client.subscribe(topic)
+    client.on_message = on_message
 
-    Temperature = randint(10, 50)
-    Humidity = randint(200, 999)
 
-    print(Temperature, Humidity)
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_start()
 
-    Temp.set_value(Temperature)
-    Hum.set_value(Humidity)
 
-    time.sleep(2)
+if __name__ == "__main__":
+
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://0.0.0.0:4840/UA/rasp")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://examples.freeopcua.github.io"
+    idx = server.register_namespace(uri)
+
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = objects.add_object(idx, "MyObject")
+    myvar = myobj.add_variable(idx, "Temp", 6.7)
+    myvar.set_writable()  # Set MyVariable to be writable by clients
+
+    run()
+
+    # starting!
+    server.start()
+
+    try:
+        count = 0
+        while True:
+            time.sleep(1)
+            myvar.set_value(float(message))
+            print(float(message))
+
+    finally:
+        # close connection, remove subcsriptions, etc
+        server.stop()
